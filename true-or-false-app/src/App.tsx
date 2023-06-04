e92c5e05266e323178b161f3457d8f0b606ae2a4
sayanok <rng.jhn.osca@gmail.com>
2023-06-04 14:30:22 +0900

    addFunction

--- true-or-false-app/src/App.tsx
+++ true-or-false-app/src/App.tsx
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App: React.FC = () => {
+  const [formA, setFormA] = useState("");
+  const [formB, setFormB] = useState("");
+  const [conditionalOperator, setConditionalOperator] = useState("");
+  const [result, setResult] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  function onClickHandler() {
+    setErrorMessage("");
+
+    if (conditionalOperator === "===") {
+      setResult((formA === formB).toString());
+    } else if (conditionalOperator === "<") {
+      setResult((formA < formB).toString());
+    } else if (conditionalOperator === ">") {
+      setResult((formA > formB).toString());
+    } else if (conditionalOperator === "<=") {
+      setResult((formA <= formB).toString());
+    } else if (conditionalOperator === ">=") {
+      setResult((formA >= formB).toString());
+    } else if (conditionalOperator === "==") {
+      setResult((formA == formB).toString());
+    } else if (conditionalOperator === "!==") {
+      setResult((formA !== formB).toString());
+    } else {
+      setErrorMessage("演算子が無効です。正しい演算子を入力してください。");
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p>比較したいものを入力してください</p>
+      <textarea id="formA" onChange={(e) => setFormA(e.target.value)}>
+        {formA}
+      </textarea>
+      <textarea id="formB" onChange={(e) => setFormB(e.target.value)}>
+        {formB}
+      </textarea>
+      <p>比較方法(javascript演算子)を入力してください</p>
+      <textarea onChange={(e) => setConditionalOperator(e.target.value)}>{conditionalOperator}</textarea>
+      <button onClick={() => onClickHandler()}>比較する</button>
+      <p>{errorMessage}</p>
+      <p>結果: {result}</p>
+    </>
   );
-}
+};
 
 export default App;
